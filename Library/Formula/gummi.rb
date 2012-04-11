db27ab0fb5c15db59f84aba1ab6d3641b4b05855
Kyle Harrigan <kwharrigan@gmail.com>
2012-04-10 20:42:41 -0400

    Formula for gummi... doesn't work yet.  Issues with gtk

---
+++ Library/Formula/gummi.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gummi < Formula
+  homepage 'http://dev.midnightcoding.org/projects/gummi'
+  url 'http://dev.midnightcoding.org/attachments/download/263/gummi-0.6.3.tar.gz'
+  md5 '6d02259d237c35c149d6a5f61f76ad3e'
+
+  # depends_on 'cmake' => :build
+  depends_on 'gettext'
+  depends_on 'gtksourceview'
+
+  def install
+    # ENV.x11 # if your formula requires any X11 headers
+    # ENV.j1  # if your formula's build system can't parallelize
+
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    # system "cmake . #{std_cmake_parameters}"
+    system "make install" # if this fails, try separate make/make install steps
+  end
+
+  def test
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test gummi`.
+    system "false"
+  end
+end
